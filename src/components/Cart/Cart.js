25a0c5b352d2dc8e1d1c63ad33ed47b275cc8d6b
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-09-26 23:31:36 +0600

    Saving

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,10 +4,12 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
   let total = 0;
   let shipping = 0;
+  let quantity = 0;
 
   for (const product of cart) {
-    total += product.price;
-    shipping += product.shipping;
+    quantity += product.quantity;
+    total += product.price * product.quantity;
+    shipping += product.shipping * product.quantity;
   }
 
   const tax = parseFloat((total * 0.1).toFixed(2));
@@ -15,7 +17,7 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${total}</p>
       <p>Shipping: ${shipping}</p>
       <p>Tax: ${tax}</p>
