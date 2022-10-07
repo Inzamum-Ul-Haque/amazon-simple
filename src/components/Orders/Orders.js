aa8472c7b811ea379a2a376bb1e96cc0233bb996
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-10-07 23:31:31 +0600

    Saving to Git

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,16 +1,31 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
-  const { products, previousCart } = useLoaderData();
+  const { previousCart } = useLoaderData();
   const [cart, setCart] = useState(previousCart);
-  console.log(cart);
+
+  const handleRemoveItem = (id) => {
+    const remainingItems = cart.filter((product) => product.id !== id);
+    setCart(remainingItems);
+    removeFromDb(id);
+  };
 
   return (
     <div>
       <div className="shop-container">
-        <div className="products-container"></div>
+        <div className="orders-container">
+          {cart.map((product) => (
+            <ReviewItem
+              key={product.id}
+              product={product}
+              handleRemoveItem={handleRemoveItem}
+            />
+          ))}
+        </div>
         <div className="cart-container">
           <Cart cart={cart} />
         </div>
